304880709373dd7fb66cd73fe2cc3fc460de818a
Marc Bermejo <markBETA@users.noreply.github.com>
2020-01-11 07:01:31 +0100

    Updated production config

--- instance/production.py
+++ instance/production.py
@@ -12,17 +12,17 @@ class Config(_Config):
     ENV = "production"
 
     SQLALCHEMY_BINDS = {
-        'app': 'postgresql+psycopg2://postgres:bcnraprep@queue-manager-database.c8lme8jgnxxk.eu-west-3.rds.amazonaws.com/app',
-        'auth': 'postgresql+psycopg2://postgres:bcnraprep@queue-manager-database.c8lme8jgnxxk.eu-west-3.rds.amazonaws.com/auth'
+        'app': 'postgresql+psycopg2://postgres:7T#uZqxfL[z%,GGA@queue-manager-database.cdsfc1sk270d.eu-west-1.rds.amazonaws.com/app',
+        'auth': 'postgresql+psycopg2://postgres:7T#uZqxfL[z%,GGA@queue-manager-database.cdsfc1sk270d.eu-west-1.rds.amazonaws.com/auth'
     }
 
-    REDIS_SERVER_HOST = 'queue-manager-cache.4mhe1r.ng.0001.euw3.cache.amazonaws.com'
+    REDIS_SERVER_HOST = 'queue-manager-redis.kklcm3.0001.euw1.cache.amazonaws.com'
 
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30)
     JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=30)
-    with open("keys/jwt.key", "r") as f:
+    with open("/etc/authserver/keys/jwt.key", "r") as f:
         JWT_PRIVATE_KEY = f.read()
-    with open("keys/jwt.key.pub", "r") as f:
+    with open("/etc/authserver/keys/jwt.key.pub", "r") as f:
         JWT_PUBLIC_KEY = f.read()
 
     CORS_ALLOWED_ORIGINS = ["http://queuemanagerbcn3d.ml", "http://www.queuemanagerbcn3d.ml"]
